506dab902cf830a66c3dd33d9aa8956b058954a9
Youjia Li <lyoujia17@gmail.com>
2024-09-05 14:37:18 -0500

    mute print info

--- src/dispatchers/file.c
+++ src/dispatchers/file.c
@@ -1718,8 +1718,8 @@ ncmpi_enddef(int ncid) {
     MPI_Offset malloc_size;
     //Check memory usage
     err = ncmpi_inq_malloc_size(&malloc_size);
-    if (rank == 0)
-        printf("\nBefore sort: heap memory allocated by PnetCDF internally has %.2f MB  yet to be freed\n",(double)malloc_size / (1024 * 1024));
+    // if (rank == 0)
+        // printf("\nBefore sort: heap memory allocated by PnetCDF internally has %.2f MB  yet to be freed\n",(double)malloc_size / (1024 * 1024));
     int **dim_sort_map = (int **)NCI_Malloc(nproc * sizeof(int *));
     int **var_sort_map = (int **)NCI_Malloc(nproc * sizeof(int *));
     struct hdr** all_recv_hdr = (struct hdr**)NCI_Malloc(nproc * sizeof(struct hdr*));
@@ -1779,8 +1779,8 @@ ncmpi_enddef(int ncid) {
         }
     //Check memory usage
     err = ncmpi_inq_malloc_size(&malloc_size);
-    if (rank == 0)
-        printf("\nAfter sort: heap memory allocated by PnetCDF internally has %.2f MB  yet to be freed\n",(double)malloc_size / (1024 * 1024));
+    // if (rank == 0)
+        // printf("\nAfter sort: heap memory allocated by PnetCDF internally has %.2f MB  yet to be freed\n",(double)malloc_size / (1024 * 1024));
     //start construct the combined hdr structure
     for (int i = 0; i < nproc; ++i) {
         // struct hdr *recv_hdr = (struct hdr*)NCI_Malloc(sizeof(struct hdr));
