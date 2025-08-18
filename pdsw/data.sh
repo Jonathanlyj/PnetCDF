bf2e64eec8495e60a1e48bebd46f786f60f4c8dc
Youjia Li <lyoujia17@gmail.com>
2025-08-17 23:47:06 -0500

    remove old folder

--- pdsw/data.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p data
-tar -xzvf dataset_568k_metadata.tar.gz -C data/
-make create_ncopy_binary
-./create_ncopy_binary ./data/dataset_568k_metadata ./data/dataset_5m_metadata
--- pdsw/run_lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Testing on dataset_568k metadata:
-export PNETCDF_HINTS="nc_hash_size_dim=1048576;nc_hash_size_var=1048576"
-mpiexec -n 4 ./lib_baseline_test_all data/dataset_568k_metadata ./data/dataset_568k_metadata_lib_baseline_test_all.nc
-# Testing on dataset_5m metadata:
-# export PNETCDF_HINTS="nc_hash_size_dim=16777216;nc_hash_size_var=16777216"
-# mpiexec -n 4 ./lib_baseline_test_all data/dataset_5m_metadata ./data/dataset_5m_metadata_lib_baseline_test_all.nc
